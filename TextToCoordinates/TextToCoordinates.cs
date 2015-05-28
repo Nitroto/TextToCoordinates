7eb8e8049533e8036af2d1a24bb25bc6acaca168
Asen Todorov <astodorov@outlook.com>
2015-05-28 13:18:40 +0300

    ASCII Art to console coordinates
    convert ASCII Art to console coordinates

---
+++ TextToCoordinates/TextToCoordinates.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class TextToCoordinates
+{
+    static void Main()
+    {
+        char[][] field = new char[21][];
+        int number = 0;
+        List<string> coordinates = new List<string>();
+        using (StreamReader reader = new StreamReader("..\\..\\playfield.txt"))
+        {
+            string line = string.Empty;
+            while ((line = reader.ReadLine()) != null)
+            {
+                field[number] = line.ToCharArray();
+                number++;
+            }
+        }
+        using (StreamWriter writer = new StreamWriter("..\\..\\coordinates.txt"))
+            for (int i = 0; i < field.Count(); i++)
+            {
+                for (int j = 0; j < field[i].Count(); j++)
+                {
+                    if (field[i][j] != ' ')
+                    {
+                        string validCoordinate = string.Format("{0} {1} {2}", field[i][j], j, i);
+                        coordinates.Add(validCoordinate);
+                        writer.WriteLine(validCoordinate);
+                    }
+                }
+            }
+
+    }
+}
